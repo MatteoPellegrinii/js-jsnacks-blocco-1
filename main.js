0458949f0bad37f949eb0ab9eaf12667cd179918
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-10 16:03:46 +0200

    push

---
+++ main.js
@@ -0,0 +1,12 @@
+let firstarray = [ "parola1-1", "parola2-1", "parola3-1", "parola4-1", "parola5-1" ];
+console.log(firstarray);
+let secondarray = ["parola1-2", "parola2-2" ];
+
+function myFunction() {
+    let ul = document.getElementById('first');
+    console.log(ul);
+    let li = document.createElement('li');
+    
+}
+
+
