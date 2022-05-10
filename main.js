05f3d7f9d7da4cb24169abe62185cdcc17ef9ad2
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-10 16:47:55 +0200

    push

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 let firstarray = [ "parola1-1", "parola2-1", "parola3-1", "parola4-1", "parola5-1" ];
 
+let text = ""
+for (let i = 0; i < 4; i++) {
+  text =  firstarray[i];
+  let line = document.createElement(text);
+  console.log(line);
+  document.getElementById("first").appendChild(line);
+}
 
-let text = "<ul>";
-firstarray.forEach(myFunction);
-text += "</ul>";
-document.getElementById("first").innerHTML = text;
-
-function myFunction(value) {
-  text += "<li>" + value + "</li>";
-} 
 
 let secondarray = ["parola1-2", "parola2-2" ];
-
-let second = "<ul>";
