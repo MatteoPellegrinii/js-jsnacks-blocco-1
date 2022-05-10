a0fd0ca736f153ee4e510c2fc97dc323ec77a799
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-10 17:48:50 +0200

    push

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 let firstarray = [ "parola1-1", "parola2-1", "parola3-1", "parola4-1", "parola5-1" ];
-let secondarray = ["parola1-2", "parola2-2" ];
+
 
 for (let i = 0; i < 5; i++) {;
   let line = document.createElement("li");
   document.getElementById("first").appendChild(line);
+  line.innerHTML += firstarray[i];
+  let ul = document.createElement("ul");
+    document.getElementById("first").appendChild(ul);
+
+
+  let secondarray = ["parola1-2", "parola2-2" ];
+
+  for (let i = 0; i < 2; i++) {
+    let line = document.createElement("li");
+    document.getElementById("first").appendChild(line);
+    line.innerHTML += secondarray[i];
+    
+      
+  }
 
 }
 
