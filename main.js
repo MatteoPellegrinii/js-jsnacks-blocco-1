9aa45bc257ec9ecef4e09b8890f75c8c3b0321d6
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-12 15:18:23 +0200

    push

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 let firstarray = [ "parola1-1", "parola2-1", "parola3-1", "parola4-1", "parola5-1" ];
+let secondarray = ["parola1-2", "parola2-2" ];
+
+let ulfirst = document.getElementById("first");
+
+for (let i = 0; i<firstarray.length; i++){
+  let firstarraycontent = firstarray[i];
+  let firstli = document.createElement("li");
+  firstli.append(firstarraycontent);
+  ulfirst.append(firstli);
 
 
-for (let i = 0; i < 5; i++) {;
-  let line = document.createElement("li");
-  document.getElementById("first").appendChild(line);
-  line.innerHTML += firstarray[i];
-  let ul = document.createElement("ul");
-    document.getElementById("first").appendChild(ul);
+  let ulsecond = document.createElement("ul");
 
-
-  let secondarray = ["parola1-2", "parola2-2" ];
-
-  for (let i = 0; i < 2; i++) {
-    let line = document.createElement("li");
-    document.getElementById("first").appendChild(line);
-    line.innerHTML += secondarray[i];
-    
-      
+  for (let j = 0; j < secondarray.length; i++){
+    let secondarraycontent = secondarray[i];
+    let secondli = document.createElement("li");
+    secondli.append(secondarraycontent);
+    ulsecond.append(secondli);
+    firstli.append(ulsecond);
   }
-
 }
 
 
